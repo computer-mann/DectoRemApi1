54780e755882ba38010c268f62353bfd333b441b
computer-mann <hpsn16@gmail.com>
2019-10-23 15:19:31 +0000

    api keys

--- Controllers/SendMailController.cs
+++ Controllers/SendMailController.cs
@@ -18,7 +18,7 @@ namespace DecToRem.Controllers
         
 
         private readonly ILogger<SendMailController> _logger;
-        private readonly string apiKey="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";
+        private readonly string apiKey="SG.-FqY2_iyRWS3xs5Ea1Ef_g.qbFvWdsFmP3QfYTn99e1SaxzG4czKaRd71OlX53od0YY";
         //private readonly string TEMPLATE_ID="d-28d226c381c843ca9639a86ea827dd28";
 
         public SendMailController(ILogger<SendMailController> logger)
@@ -31,9 +31,9 @@ namespace DecToRem.Controllers
        	if(mail==null) return BadRequest();
             var msg =new SendGridMessage();
             var client = new SendGridClient(apiKey);
-            msg.SetFrom( new EmailAddress("quarshie.wendyvi@gmail.com", "Sam TV"));
+            msg.SetFrom( new EmailAddress("quadsrshie.wendyfgvi@gmail.com", "Sam TV"));
             msg.AddTo(new EmailAddress(mail.Email,mail.Email));
-            msg.SetTemplateId("d-28d226c381c843ca9639a86ea827dd28");
+            msg.SetTemplateId("d-28d226c381c843ca9639a8sd6ea827dd28");
             var dynamicTemplateData = new TemplateData()
             {
                Name=mail.FirstName,
